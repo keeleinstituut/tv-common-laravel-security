5a6bc3c070c4237e10c865927ce6e3af65255056
Dan Bondarenko <dan.bondarenko.1@gmail.com>
2023-05-09 17:13:09 +0300

    feat(#111): specify HTTP status codes in exceptions

--- src/Exceptions/InvalidJwtTokenException.php
+++ src/Exceptions/InvalidJwtTokenException.php
@@ -3,7 +3,18 @@
 namespace KeycloakAuthGuard\Exceptions;
 
 use Nette\InvalidArgumentException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
-class InvalidJwtTokenException extends InvalidArgumentException
+class InvalidJwtTokenException extends InvalidArgumentException implements HttpExceptionInterface
 {
+    public function getStatusCode(): int
+    {
+        return Response::HTTP_FORBIDDEN;
+    }
+
+    public function getHeaders(): array
+    {
+        return [];
+    }
 }
